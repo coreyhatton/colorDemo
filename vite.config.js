368e57b72ef092b3061022bd46aed98a1f633d5d
Corey Hatton <corey@coreyhatton.au>
2025-01-09 14:51:42 +0800

    fixed vite base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/", // Set the base URL for your GitHub Pages site
+  base: "/colorDemo/", // Set the base URL for your GitHub Pages site
   plugins: [react()],
 });

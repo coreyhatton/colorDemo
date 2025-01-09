da9a5daf1090b496a069a697dd6f8ee20aaaed85
Corey Hatton <corey@coreyhatton.au>
2025-01-09 14:42:38 +0800

    Update GitHub Actions workflow for deployment and remove deploy script

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/coreyhatton.github.io/", // Set the base URL for your GitHub Pages site
+  base: "/", // Set the base URL for your GitHub Pages site
   plugins: [react()],
 });
